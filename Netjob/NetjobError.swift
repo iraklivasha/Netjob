df87bdd657b7574775f89e237f3fef2b2856759b
Irakli Vashakidze <iraklivashaka@gmail.com>
2024-01-18 13:41:01 +0400

    Better error handling

--- Netjob/NetjobError.swift
+++ Netjob/NetjobError.swift
@@ -7,22 +7,25 @@
 //
 
 import Foundation
-
 public enum NetjobError: Error {
     case requestFailed(_ error: Error?)
     case timeout(error: Error?)
     case notFound(error: Error)
-    case unauthorized(error: Error)
+    case unauthorized
     case badRequest(error: Error)
     case decodingFailed(error: Error)
     case prohibited(error: Error)
+    case user(message: String)
+    case server(error: Error)
+    case cancelled
+    case unknown(error: Error?)
     
-    init(statusCode: Int, error: Error) {
+    init(statusCode: Int, error: Error, message: String? = nil) {
         switch statusCode {
         case 400:
             self = .badRequest(error: error)
         case 401:
-            self = .unauthorized(error: error)
+            self = .unauthorized
         case 403:
             self = .prohibited(error: error)
         case 404:
@@ -31,8 +34,16 @@ public enum NetjobError: Error {
             self = .decodingFailed(error: error)
         case 408:
             self = .timeout(error: error)
-        default:
+        case 500:
+            self = .server(error: error)
+        case 1000:
             self = .requestFailed(error)
+        case 1001:
+            self = .user(message: message ?? "")
+        case -999:
+            self = .cancelled
+        default:
+            self = .unknown(error:error)
         }
     }
     
@@ -45,6 +56,26 @@ public enum NetjobError: Error {
         case .badRequest(_): return 400
         case .decodingFailed(_): return 422
         case .requestFailed(_): return 1000
+        case .cancelled: return -999
+        case .user(_): return 1001
+        case .server(_): return 500
+        case .unknown(_): return 1002
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .unknown:          return "Something went wrong"
+        case .notFound:         return "Resource not found"
+        case .prohibited:       return "Forbidden"
+        case .server:           return "Internal server error"
+        case .cancelled:        return "Cancelled"
+        case .unauthorized:     return "Unauthorized access"
+        case .decodingFailed:   return "Invalid content"
+        case .timeout:          return "Network timeout"
+        case .requestFailed:    return "Request failed"
+        case .badRequest:       return "Bad request"
+        case .user(let m):      return m
         }
     }
 }
